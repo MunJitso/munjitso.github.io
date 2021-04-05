6b62da37c0f0dd06fd4bbc3c10937a976a7b53db
MunJitso <72764494+MunJitso2005@users.noreply.github.com>
2021-04-05 14:39:50 +0200

    Add files via upload

--- portfolio/main.js
+++ portfolio/main.js
@@ -2,6 +2,7 @@ let darkBackground = document.querySelector('body');
 let darkModeBtn = document.getElementById('darkModeBtn');
 let btnIcon = document.getElementById('btnIcon');
 let changeNavbarItem = document.getElementById('navbarDark');
+let codeButton = document.getElementsByClassName('code-btn');
 
 darkModeBtn.addEventListener('click', function() {
     darkBackground.classList.toggle('darkbackground');
@@ -9,4 +10,9 @@ darkModeBtn.addEventListener('click', function() {
     btnIcon.classList.toggle('fa-moon');
     changeNavbarItem.classList.toggle('navbar-dark');
     changeNavbarItem.classList.toggle('navbar-light');
-})
+    for (var i = 0, len = codeButton.length; len > i; i++) {
+        codeButton[i].classList.toggle('btn-outline-light');
+        codeButton[i].classList.toggle('btn-outline-dark');
+
+    };
+});
